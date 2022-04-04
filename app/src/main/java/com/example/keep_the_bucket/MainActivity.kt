d3d7e42baf56a1f604f5dcaaef31132ee83353ad
kdy1242 <dy12311229@gmail.com>
2022-04-05 02:35:27 +0900

    bottom navigation-test

--- app/src/main/java/com/example/keep_the_bucket/MainActivity.kt
+++ app/src/main/java/com/example/keep_the_bucket/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.keep_the_bucket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val bucketListFragment = BucketListFragment()
+        val usersFragment = UsersFragment()
+        val profileFragment = ProfileFragment()
+
+        replaceFragment(homeFragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(homeFragment)
+                R.id.bucket_list -> replaceFragment(bucketListFragment)
+                R.id.users -> replaceFragment(usersFragment)
+                R.id.setting -> replaceFragment(profileFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        // 현 Activity 에 연결된 Fragment 관리하는 supportFragmentManager 를 통해 Fragment 전환
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
+        }
     }
 }
