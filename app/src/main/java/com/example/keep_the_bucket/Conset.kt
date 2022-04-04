5cd43a72529d239fa903367506b566946fa31b81
효정이 <w2029@e-mirim.hs.kr>
2022-04-05 01:01:06 +0900

    password show change

---
+++ app/src/main/java/com/example/keep_the_bucket/Conset.kt
@@ -0,0 +1,15 @@
+package com.example.keep_the_bucket
+
+import android.os.Bundle
+import android.widget.CheckBox
+import androidx.appcompat.app.AppCompatActivity
+
+class Conset : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consent)
+
+        var checkBtn : CheckBox = findViewById(R.id.checkbox);
+        checkBtn.bringToFront();
+    }
+}
