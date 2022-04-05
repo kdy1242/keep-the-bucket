06d54ceb6518e2b1f0f97c84ea94e537e8cefdd3
c04nh <w2038@e-mirim.hs.kr>
2022-04-05 19:44:01 +0900

    add Profile.kt

---
+++ app/src/main/java/com/example/keep_the_bucket/Profile.kt
@@ -0,0 +1,10 @@
+package com.example.keep_the_bucket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
