1c6b32d49ee05f9ecdebc3170b252f61af98b4bd
효정이 <w2029@e-mirim.hs.kr>
2022-04-08 01:03:00 +0900

    버튼 클릭 시 색상 변경

--- app/src/main/java/com/example/keep_the_bucket/Conset.kt
+++ app/src/main/java/com/example/keep_the_bucket/Conset.kt
@@ -1,6 +1,11 @@
 package com.example.keep_the_bucket
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 
--- app/src/main/java/com/example/keep_the_bucket/Login.kt
+++ app/src/main/java/com/example/keep_the_bucket/Login.kt
@@ -1,11 +1,27 @@
 package com.example.keep_the_bucket
-
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 
-class Login : AppCompatActivity() {
+import androidx.fragment.app.Fragment
+
+class Login : Fragment(R.layout.activity_login) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_join, container, false)
+        val btn : Button = view.findViewById(R.id.btn)
+
+        return view
     }
 }
+
