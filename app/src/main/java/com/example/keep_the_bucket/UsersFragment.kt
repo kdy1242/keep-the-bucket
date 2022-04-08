4ba5b07738ebc540b1f79f3b544ee5b6fa183357
c04nh <w2038@e-mirim.hs.kr>
2022-04-08 19:53:30 +0900

    users Fragment finish

--- app/src/main/java/com/example/keep_the_bucket/UsersFragment.kt
+++ app/src/main/java/com/example/keep_the_bucket/UsersFragment.kt
@@ -1,10 +1,14 @@
 package com.example.keep_the_bucket
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.ViewFlipper
 
 
 class UsersFragment : Fragment(R.layout.fragment_users) {
@@ -19,6 +23,31 @@ class UsersFragment : Fragment(R.layout.fragment_users) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false)
+        val view = inflater.inflate(R.layout.fragment_users, container, false)
+        val userstaball : LinearLayout = view.findViewById(R.id.users_tab_all)
+        val userstabshare : LinearLayout = view.findViewById(R.id.users_tab_share)
+        val userstaballtext : TextView = view.findViewById(R.id.users_tab_all_text)
+        val usertabsharetext : TextView = view.findViewById(R.id.users_tab_share_text)
+        val userstaballview :View = view.findViewById(R.id.users_tab_all_view)
+        val usertabshareview :View = view.findViewById(R.id.users_tab_share_view)
+        val viewFlipper : ViewFlipper = view.findViewById(R.id.users_viewflipper)
+
+        userstaball.setOnClickListener(){
+            userstaballtext.setTextColor(Color.parseColor("#F8BAA0"))
+            usertabsharetext.setTextColor(Color.parseColor("#868686"))
+            userstaballview.visibility = View.VISIBLE
+            usertabshareview.visibility = View.INVISIBLE
+            viewFlipper.displayedChild = 0
+        }
+
+        userstabshare.setOnClickListener(){
+            userstaballtext.setTextColor(Color.parseColor("#868686"))
+            usertabsharetext.setTextColor(Color.parseColor("#F8BAA0"))
+            userstaballview.visibility = View.INVISIBLE
+            usertabshareview.visibility = View.VISIBLE
+            viewFlipper.displayedChild = 1
+        }
+
+        return view
     }
 }
