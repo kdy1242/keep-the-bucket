375dcbfae3033b7438de29679733748d75c78e70
jiwoo227 <w2040@e-mirim.hs.kr>
2022-06-02 22:10:42 +0900

    login, join 구현

--- app/src/main/java/com/example/keep_the_bucket/Login.kt
+++ app/src/main/java/com/example/keep_the_bucket/Login.kt
@@ -1,28 +1,63 @@
 package com.example.keep_the_bucket
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
-class Login : Fragment(R.layout.activity_login) {
-
+class Login : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
+
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val btn = findViewById<Button>(R.id.btn)
+        val join = findViewById<Button>(R.id.join)
+
+        join.setOnClickListener{
+            val intent = Intent (this, Join::class.java)
+            startActivity(intent)
+        }
+
+        btn.setOnClickListener{
+            signinEmail()
+        }
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.activity_join, container, false)
-        val btn : Button = view.findViewById(R.id.btn)
-
-        return view
+    fun signinEmail() {
+        auth?.signInWithEmailAndPassword(email.text.toString(),password.text.toString())
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful) {
+                    // Login, 아이디와 패스워드가 맞았을 때
+                    moveMainPage(task.result?.user)
+                } else {
+                    // Show the error message, 아이디와 패스워드가 틀렸을 때
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
     }
+
+    private fun moveMainPage(user: FirebaseUser?) {
+        // 파이어베이스 유저 상태가 있을 경우 다음 페이지로 넘어갈 수 있음
+        if(user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
 }
 
