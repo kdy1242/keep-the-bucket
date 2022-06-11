05307bcf7a1b6704aabe916be1bd245b385d6753
효정이 <w2029@e-mirim.hs.kr>
2022-06-12 04:29:37 +0900

    뒤로가기 구현

--- app/src/main/java/com/example/keep_the_bucket/DiaryActivity.kt
+++ app/src/main/java/com/example/keep_the_bucket/DiaryActivity.kt
@@ -1,11 +1,28 @@
 package com.example.keep_the_bucket
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class DiaryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        var backbtn = findViewById<ImageView>(R.id.back_btn)
+
+        backbtn.setOnClickListener {
+            val intent = Intent(this, FragmentList::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
     }
 }
